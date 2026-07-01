c90a1c67d038ab178ee3cac352cf1a31c155d88d
PhilipTodd <philip.todd@gmail.com>
2026-07-01 13:08:50 +1000

    ADd shell

--- src/frontend/blast-planning-ui/src/app/app.ts
+++ src/frontend/blast-planning-ui/src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { AppShell } from './layout/app-shell/app-shell';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [AppShell],
+  template: '<app-app-shell />',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('blast-planning-ui');
-}
+export class App { }
--- src/frontend/blast-planning-ui/src/app/layout/app-shell/app-shell.ts
+++ src/frontend/blast-planning-ui/src/app/layout/app-shell/app-shell.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-app-shell',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './app-shell.html',
-  styleUrl: './app-shell.scss',
+  styleUrl: './app-shell.scss'
 })
-export class AppShell {
-
-}
+export class AppShell { }
