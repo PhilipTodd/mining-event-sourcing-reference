95df635e2aba2d0ff4550480ec1651ee2d53c488
PhilipTodd <philip.todd@gmail.com>
2026-07-01 13:45:43 +1000

    Add About page

--- src/frontend/blast-planning-ui/src/app/pages/about/about.ts
+++ src/frontend/blast-planning-ui/src/app/pages/about/about.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule
+  ],
   templateUrl: './about.html',
-  styleUrl: './about.scss',
+  styleUrl: './about.scss'
 })
-export class About {
-
-}
+export class About { }
