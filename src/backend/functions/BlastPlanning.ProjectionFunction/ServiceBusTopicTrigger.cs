c1d1760086691ea72512cc754e1f01e404eb07c2
PhilipTodd <philip.todd@gmail.com>
2026-08-05 12:28:27 +1000

    Create Azure Function project scaffold

---
+++ src/backend/functions/BlastPlanning.ProjectionFunction/ServiceBusTopicTrigger.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace BlastPlanning.ProjectionFunction;
+
+public class ServiceBusTopicTrigger
+{
+    private readonly ILogger<ServiceBusTopicTrigger> _logger;
+
+    public ServiceBusTopicTrigger(ILogger<ServiceBusTopicTrigger> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(ServiceBusTopicTrigger))]
+    public async Task Run(
+        [ServiceBusTrigger("mytopic", "mysubscription", Connection = "")]
+        ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions)
+    {
+        _logger.LogInformation("Message ID: {id}", message.MessageId);
+        _logger.LogInformation("Message Body: {body}", message.Body);
+        _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+
+            // Complete the message
+        await messageActions.CompleteMessageAsync(message);
+    }
+}
